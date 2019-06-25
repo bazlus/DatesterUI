16c44d6a7221568539e7d40360a037240d948997
Boyan Mihaylov <boyan.mihaylov@naxex-tech.com>
2019-06-25 17:23:25 +0300

    Added sidebar and logo

--- DatesterUI/src/app/app.module.ts
+++ DatesterUI/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { SidebarModule } from 'primeng/sidebar';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ButtonModule } from 'primeng/button';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent,
+    MenuComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SidebarModule,
+    BrowserAnimationsModule,
+    ButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
